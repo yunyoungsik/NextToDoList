5f55973af87dd2ec58537cde02ce27b324d4ceff
nason <nason@vanishst.com>
2024-07-10 16:38:13 +0900

    [20240710]동적라우팅 완료

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,41 @@
-'use client'
+'use client';
 
 import { useRouter } from 'next/navigation';
+import { supabase } from '@/utils/supabase';
+// Shadch UI
 import { Button } from '@/components/ui/button';
+import { toast } from '@/components/ui/use-toast';
+// CSS
 import styles from './page.module.scss';
 
 function Home() {
   const router = useRouter();
+
+  // 페이지 생성 및 Supabase 연동
+  const onCreate = async () => {
+    // Supabase 데이터베이스 row 생성
+    const { error, status } = await supabase
+      .from('todos')
+      .insert([{ title: '', start_date: new Date(), end_date: new Date(), contents: [] }])
+      .select();
+
+    if (error) {
+      console.log(error);
+    }
+    if (status === 201) {
+      toast({
+        title: '생성 완료!',
+        description: '새로운 투두리스트가 생성 되었습니다.',
+      });
+    }
+
+    // 방금 생성한 TO DO LIST의 ID 값으로 URL 파라미터 생성
+    let { data } = await supabase.from('todos').select();
+
+    if (data) {
+      router.push(`/create/${data[data?.length - 1].id}`);
+    } else return;
+  };
 
   return (
     <div className={styles.container}>
@@ -21,7 +51,7 @@ function Home() {
         <Button
           variant="outline"
           className="w-full bg-transparent text-orange-500 border-orange-500 hover:bg-orange-50 hover:text-orange-500"
-          onClick={() => router.push("/create")}
+          onClick={onCreate}
         >
           Add New page
         </Button>
